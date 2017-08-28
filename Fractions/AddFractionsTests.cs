1a2b3f8f4950e765bcafce8f42e43324489df649
Joakim Rodling <joakim.rodling@nepa.se>
2017-08-28 15:01:41 +0200

    Initial commit

---
+++ Fractions/AddFractionsTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fractions
+{
+    [TestClass]
+    public class AddFractionsTests
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    Assert.AreEqual(1, 1);
+        //}
+    }
+}
