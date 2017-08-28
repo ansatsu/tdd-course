d0188f9418a85b7fcc653c047fd2da5526ad30e0
Joakim Rodling <joakim.rodling@nepa.se>
2017-08-28 16:19:50 +0200

    Made test pass

--- Fractions/AddFractionsTests.cs
+++ Fractions/AddFractionsTests.cs
@@ -18,5 +18,12 @@ namespace Fractions
             var fraction = new Fraction(0).Plus(new Fraction(1));
             Assert.AreEqual(1, fraction.IntValue);
         }
+
+        [TestMethod]
+        public void nonzero_plus_zero()
+        {
+            var fraction = new Fraction(2).Plus(new Fraction(0));
+            Assert.AreEqual(2, fraction.IntValue);
+        }
     }
 }
