71a3b57210feb2e243ece9f4630c9d79a0b5a042
Joakim Rodling <joakim.rodling@nepa.se>
2017-09-01 08:47:30 +0200

    Equals tests for differing numerators and denominators.

---
+++ Fractions/FractionEqualsTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fractions
+{
+    [TestClass]
+    public class FractionEqualsTests
+    {
+        [TestMethod]
+        public void same_numerator_and_denominator()
+        {
+            Assert.AreEqual(new Fraction(3, 4), new Fraction(3, 4));
+        }
+
+        [TestMethod]
+        public void different_numerators()
+        {
+            Assert.AreNotEqual(new Fraction(2, 3), new Fraction(5, 3));
+        }
+
+        [TestMethod]
+        public void different_denominators()
+        {
+            Assert.AreNotEqual(new Fraction(4, 5), new Fraction(4, 8));
+        }
+    }
+}
